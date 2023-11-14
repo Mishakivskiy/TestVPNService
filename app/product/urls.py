43e9c3595769b33551202e362b99858d4fd98dfc
ivan <ivan.mishakivskiy@gearheart.io>
2023-11-15 01:05:30 +0200

    mvp 2

--- app/product/urls.py
+++ app/product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, profile
+from .views import home, profile, edit_profile
 
 urlpatterns = [
     path('', home, name='home'),
     path('profile/', profile, name='profile'),
+    path('profile/edit/', edit_profile, name='edit_profile'),
 ]
--- app/product/views.py
+++ app/product/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import EditProfileForm
 
 
 def home(request):
     return render(request, 'product/home.html', {'user': request.user})
 
 
+@login_required
 def profile(request):
-    return render(request, 'product/profile.html')
+    return render(request, 'product/profile.html', {'user': request.user})
+
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = EditProfileForm(instance=request.user)
+    return render(request, 'product/edit_profile.html', {'form': form})
 
